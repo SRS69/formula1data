878b15d3ffecf06665091eb91c2445d28a453a14
SRS69 <95501872+SRS69@users.noreply.github.com>
2022-04-23 14:16:10 +0000

    Aggiunto Tailwind CSS e DaisyUI (libreria Tailwind di componenti)

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+
+   daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "light",
+  },
+}
